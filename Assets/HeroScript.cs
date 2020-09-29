2899bdf61e2d933f448244391cd53a54dbae0f01
ZachZ <ZachZ@users.noreply.github.com>
2020-09-28 23:00:15 -0600

    Initial Commit

---
+++ Assets/HeroScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+
+    int pSpeed;
+    int pHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pSpeed == null)
+        {
+            pSpeed = 5;
+        }   
+
+        if (pHealth == null)
+        {
+            pHealth = 3;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
