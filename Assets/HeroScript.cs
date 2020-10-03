a62d5002a4977a38699f3e29dbf4521d95862dd4
ZachZ <ZachZ@users.noreply.github.com>
2020-10-02 18:40:09 -0600

    Initial engine work
    character movement
    doors

--- Assets/HeroScript.cs
+++ Assets/HeroScript.cs
@@ -5,29 +5,142 @@ using UnityEngine;
 public class HeroScript : MonoBehaviour
 {
 
+    //object ref
+    public GameObject TopDoor;
+    public GameObject BottomDoor;
+
+    //movement var
+    [SerializeField]
     int pSpeed;
-    int pHealth;
+
+    Vector2 movement;
+
+    [SerializeField]
+    Rigidbody2D myRB;
+
+
+    //Pause menu var
+    bool gaming;
+    bool pausedGame;
+
+    [SerializeField]
+    public GameObject pauseMenu;
+
+    bool doorOpen;
+    float doorTimer;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        gaming = true; 
+
         if (pSpeed == 0)
         {
             pSpeed = 5;
-        }   
-
-        if (pHealth == 0)
-        {
-            pHealth = 3;
         }
+
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        Controls();
+        DoorReset();
+    }
+
+    void FixedUpdate()
+    {
+
+        myRB.MovePosition(myRB.position + movement * pSpeed * Time.fixedDeltaTime);
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
         {
-            //move up
+            //door
+            if (col.gameObject.name == "Top Door" && doorOpen == true)
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position = BottomDoor.transform.position;
+                }
+            }
+
+            if (col.gameObject.name == "Bottom Door")
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position = TopDoor.transform.position;
+                }
+            }
         }
     }
+
+    void Controls()
+    {
+        //checks if you have game over or not
+        if (gaming)
+        {
+       
+            Movement();
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                //interact
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                pausedGame = !pausedGame;
+                PauseGame();
+            }
+        }
+
+    }
+
+    void Movement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    public void PauseGame()
+    {
+        if (pausedGame)
+        {
+            //unpause
+            Time.timeScale = 1f;
+            pauseMenu.SetActive(false);
+            pausedGame = true;
+        }
+        else
+        {
+            //pause 
+            Time.timeScale = 0.0f;
+            pauseMenu.SetActive(true);
+            pausedGame = false;
+        }
+    }
+
+    public void SetPlaying(bool gameOver)
+    {
+        gaming = gameOver;
+    }
+
+    void DoorReset()
+    {
+        doorTimer += Time.deltaTime;
+        if (doorTimer > 2.0f)
+        {
+            doorOpen = true;
+        }
+        else
+        {
+            doorOpen = false;
+        }
+    }
+
+
 }
