2a2c95be5f686ade94f2129f0252e25eaa6e5b40
sneakysushi <39606437+sneakysushi@users.noreply.github.com>
2020-10-02 19:51:15 -0600

    added script for player object interaction
    attach to player

---
+++ Assets/Script/Player Object Interaction.cs
@@ -0,0 +1,65 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerObjectInteraction : MonoBehaviour
+{
+    public int punkProgress=1;
+    public int dndProgress=1;
+    public int gamejamProgress=1;
+    public int prepProgress = 1;
+    public int energy = 1;
+    public bool hasKeys;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider col) //each item will have a collider in the space you would move to pick it up
+    {
+        var centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+        GUI.Label(Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "Press F to pick up", centeredStyle);
+        if (Input.GetKey(KeyCode.F)
+        {
+            if (col.gameObject.tag == "punkItem")
+            {
+                punkProgress += 1;
+            }
+            if (col.gameObject.tag == "dndItem")
+            {
+                dndProgress += 1;
+            }
+            if (col.gameObject.tag == "gamejamItem")
+            {
+                gamejamProgress += 1;
+            }
+            if (col.gameObject.tag == "prepItem")
+            {
+                prepProgress += 1;
+            }
+            if (col.gameObject.tag == "energyItem")
+            {
+                energy += 1;
+            }
+            if (col.gameObject.tag == "keys")
+            {
+                hasKeys = true;
+            }
+
+            Destroy(col.gameObject) //if player gets destroyed instead of item, this is the reason
+                //play pick up sound, maybe add 
+        }
+    }
+}
