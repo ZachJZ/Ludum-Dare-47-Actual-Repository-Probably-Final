712e71a905a08a24c726b2d3b13e5cbfcf2ef4a6
ZachZ <ZachZ@users.noreply.github.com>
2020-10-06 14:33:24 -0600

    final commit
    made ui changes to fit html5 build

---
+++ Assets/audioSet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioSet : MonoBehaviour
+{
+    float myVol;
+
+    AudioSource myAMP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (myVol == 0)
+        {
+            myVol = 0.5f;
+        }
+        myAMP = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myAMP.volume != myVol)
+        {
+            myAMP.volume = myVol;
+        }
+    }
+
+    public void SetVol(float seTo)
+    {
+        myVol = Mathf.Clamp(seTo, 0, 1);
+    }
+
+}
